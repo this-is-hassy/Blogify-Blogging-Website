66cade88d9d334f65d561435b9660c97d2ad15dd
Hassan Hameed <hh2910097@gmail.com>
2024-07-13 03:11:42 +0500

    limit change

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -29,7 +29,7 @@ export class Service{
                 }
             )
         } catch (error) {
-            console.log("Appwrite serive :: createPost :: error", error);
+            console.log("Appwrite service :: createPost :: error", error);
         }
     }
 
--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -17,7 +17,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
-            height: 500,
+            height: 1000,
             menubar: true,
             plugins: [
                 "image",

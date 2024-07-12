90cd2b7cfaba01719998cb2f3b55f2e09cf53c98
Hassan Hameed <hh2910097@gmail.com>
2024-07-13 03:57:47 +0500

    changes

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -5,16 +5,7 @@ import conf from '../conf/conf.js';
 
 
 export default function RTE({name, control, label, defaultValue =""}) {
-  const handleEditorChange = (editor) => {
-    // Ensure the character limit is not exceeded
-    const plainTextContent = editor.getContent({ format: 'text' });
-    if (plainTextContent.length > 2000) {
-      const truncatedContent = plainTextContent.substring(0, 2000);
-      editor.setContent(truncatedContent);
-      alert('Character limit exceeded! Only 2000 characters are allowed.');
-    }
-  };
-
+  
   return (
     <div className='w-full'> 
     {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
@@ -53,10 +44,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
             ],
             toolbar:
             "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
-            setup: (editor) => {
-              editor.on('keyup', () => handleEditorChange(editor.getContent(), editor));
-            },
+            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
         }}
         onEditorChange={onChange}
         />
